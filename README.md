9955c4bd9bf704b7a879b1fe3dddeb31e20b83f5
jake rudlong <jrudlong@gmail.com>
2023-02-08 21:59:21 -0600

    completes README and reorders js function recursion to only redo character selection

--- README.md
+++ README.md
@@ -1,18 +1,23 @@
 ## Password Generator: Module 3 Project (Columbia Bootcamp)
-[![forthebadge](https://forthebadge.com/images/badges/uses-html.svg)](https://forthebadge.com) [![forthebadge](https://forthebadge.com/images/badges/uses-css.svg)](https://forthebadge.com) [![forthebadge](https://forthebadge.com/images/badges/uses-badges.svg)](https://forthebadge.com)
+[![forthebadge](https://forthebadge.com/images/badges/uses-html.svg)](https://forthebadge.com) [![forthebadge](https://forthebadge.com/images/badges/uses-css.svg)](https://forthebadge.com) [![forthebadge](https://forthebadge.com/images/badges/uses-js.svg)](https://forthebadge.com)[![forthebadge](https://forthebadge.com/images/badges/gluten-free.svg)](https://forthebadge.com)[![forthebadge](https://forthebadge.com/images/badges/uses-badges.svg)](https://forthebadge.com)
 # Description
-A random, secure password generator that takes input from the user to choose password length between 8-128 characters (and will generate a random # between 1 and 128 if the user does not input correctly). Next, the user will answer window prompts as to whether they want lowercase, uppercase, numbers, and special characters for the password. The generator will then randomly create a password using only the characters specified
-Screenshot of the page:
+A random, secure password generator that takes input from the user to choose password length between 8-128 characters (and will generate a random # between 1 and 128 if the user does not input correctly). Next, the user will answer window prompts as to whether they want lowercase letters, uppercase letters, numbers, and special characters for the password. The generator will then randomly create a password using only the characters specified. The function includes a safecheck to account for the user not choosing any character type in that the function will be recalled (through recursion) to ask again for the user to select which characters they would like to use.
+below is a screenshot of the thought process behind the primary function (generatePassword)
+![screenshot of project](assets/guide-sheet.jpg)
 
+To view the code used for this project, the github repo can be found @ https://github.com/JDR8888/password-generator 
 
-![screenshot of project](assets/)
+To visit the page and generate a random password  based on your own input, visit the deployed site @ https://jdr8888.github.io/password-generator/
 
+Screenshot of the page below:
+![screenshot of project](assets/screenshot.jpg)
 
 # Installation
 N/A
 # Usage
-N/A
+Generate a secure, completely randomized password including one or all of the following: lowercase and/or uppercase letters, numbers, and special characters. choose a length between 8 and 128 characters or sit back and let the program pick the length for you once you pick which characters to include.
+
 # Credits
-giphy.com; forthebadge.com; Columbia U;
+giphy.com; forthebadge.com; Columbia U; Elias Spector-Zabusky and Jared Andreassi for proofreading code and comparing ideas. 
 # License
 Please see the repo for license info
--- assets/script.js
+++ assets/script.js
@@ -11,49 +11,50 @@ function writePassword() {
 }
 
 function generatePassword() {
-    let acceptableLength = Math.floor(Math.random() * (129-8) + 8); //picks random int between 8 and 128 to autopopulate if the user doesn't input a number
-    let charCheck = 0; // will be used make sure some type of char (lowercase, numbers, etc.) is selected
-    var passwordLength = Number(prompt("how long you want your password? between 8 and 128 characters please", acceptableLength));
-    let criteria = (passwordLength < 129 && passwordLength >=8);
-    if(!criteria) { //if the user doesn't put in an acceptable length, i will reset passwordLength variable back to a random int between 8 and 128
-        passwordLength = acceptableLength;
-        alert('you failed at picking a number that matches criteria so we stepped in and handled it for you, mmmkay?');
-    }
-    var charList = "" //initiates string for our character set
+    var charList = ""; //initiates string for our character set
     var password = ""; // initaiates string for building our random password
-    if(window.confirm('do you want lowercase letters?')) {
+    let charCheck = 0; // will be used make sure some type of char (lowercase, numbers, etc.) is selected
+
+    if(window.confirm('We can do lowercase letters, how does that sound?')) {
         charList = charList + "abcdefghijklmnopqrstuvwxyz";
-        charCheck = charCheck + 1;} //adds all lowercase characters to our empty string if yes
-        
-    if(window.confirm('do you want uppercase letters?')) {
+        charCheck =+ charCheck + 1;} 
+        //adds all lowercase characters to our empty string if yes
+
+    if(window.confirm('What about uppercase letters?')) {
         charList = charList + "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         charCheck = charCheck + 1;}
     // adds all uppercase characters to our string if yes
 
-    if(window.confirm('do you want numnums??')) {
+    if(window.confirm('How do you feel about numbers ??')) {
         charList = charList + "1234567890";
         charCheck = charCheck + 1;}  
     // adds all numnums to our string if yes
-    if(window.confirm('do you want special characters?')) {
+
+    if(window.confirm('Special characters are half off today, want to try some of those?')) {
         charCheck = charCheck + 1;
         charList = charList + "!@#$%^&*()";}
-         //adds spec char to string if yes. next, need to set a for loop to iterate # of times based on password length results, so can do for loop (let i=0; i<=passwordLength; i++) and for every iteration, will generate a random number between 0 and length of charList-1 to take charList[i] and append to password, so password += charList[i] and then when the loop is finished, we should have our full password and we will return it. 
-        if (charCheck === 0) {
-            alert('you messed up, kid. you gotta pick at least one character type for your password');
-            generatePassword();
-        } 
-        var nextChar;
-        for (let i = 0; i <= passwordLength; i++) {
-            nextChar = Math.floor(Math.random() *(charList.length - 0));
+         //adds spec char to string if yes. 
+         //time for quality control. if the user picked at least one of the different character sets available, charCheck will be > 0 and we will just move past the next conditional statement. however, if the user didn't pick any of the options they will have to start over, and we go back to the beginning of the function. 
+    if (charCheck === 0) {
+        alert("Yikes. You messed up, kid. You need to pick at least one character type for your password. Because it's your lucky day you get to keep trying until you get it right.");
+            generatePassword();} //recursion will bring us back to the start if our quality check is triggered
+
+    let acceptableLength = Math.floor(Math.random() * (129-8) + 8); //picks random int between 8 and 128 to autopopulate if the user doesn't input a number. this will only be needed if the user does not pick an appropriate length. 
+    var passwordLength = Number(prompt("How long would you like your password? Between 8 and 128 characters please. We can pick a length for you if you don't feel like it or if you forget how numbers work.", acceptableLength));
+        var criteria = (passwordLength < 129 && passwordLength >=8);
+
+//if the user doesn't put in an acceptable length, the function will reset passwordLength variable back to a random int between 8 and 128
+    if(!criteria) { 
+        passwordLength = acceptableLength;
+        alert('Wow. You failed to pick a number that matches criteria but we stepped in and handled it for you, mmmkay?'); }
+
+        var nextChar; // initialize variable to hold the index # of character list to pick from for each new character being added to the password. next - the for loop will iterate # of times equal to the length that we need our password to be (i.e. an iteration to place a random character in every single index of the password) 
+        for (var i = 0; i <= passwordLength; i++) {
+            nextChar = Math.floor(Math.random() *(charList.length - 0)); 
             password = password + charList[nextChar];
-            // console.log(nextChar);
-            // console.log(password);
     }
     
     return password;
 } 
 
 generateBtn.addEventListener("click", writePassword);
-    
-
-/* var password = document.getElementById("password")*/
